dfdbbc5eeed2fec7ef627e4ee5affe689525f4d5
Bea <bea.hernandez@datmean.com>
2018-05-19 12:34:07 +0200

    rossler attractor

---
+++ attractors/rossler.R
@@ -0,0 +1,60 @@
+library(deSolve)
+library(viridis)
+library(scatterplot3d)
+
+library(rgl)
+
+myColorRamp <- function(colors, values) {
+  v <- (values - min(values))/diff(range(values))
+  x <- colorRamp(colors)(v)
+  rgb(x[,1], x[,2], x[,3], maxColorValue = 255)
+}
+
+
+rossler <- function(t, state, parameters) {
+  with(as.list(c(state, parameters)), {
+    dX <- - Y - Z
+    dY <- X + (a * Y)
+    dZ <- b + Z * (X - c)
+    list(c(dX, dY, dZ))
+  })
+}
+
+iniState <- c(X = 0.01, Y = 1, Z = 1)
+
+a <- c(0.2)
+b <- c(0.2)
+c <- c(5.7)
+params <- data.frame(a = a, b = b, c = c)
+
+t <- seq(0.01, 10000, by = 0.01)
+cvec <- myColorRamp(c(viridis(1),viridis(2)[2]), t) 
+
+sequenceRossler <- function(t, color) {
+  out <- as.data.frame(ode(y = iniState, 
+                           times = t, 
+                           func = rossler, 
+                           parms = params[1,]))
+  
+  scatterplot3d(x=out[,2],
+                y=out[,3],
+                z=out[,4],
+                color=color,
+                type="p",
+                box=FALSE,
+                highlight.3d=F,
+                grid=F,
+                axis=F,
+                xlab=NULL,
+                ylab=NULL,
+                zlab=NULL,
+                main=NULL)
+}
+
+
+
+
+png(filename = "images/rossler1.png", width = 1200, height = 1200, pointsize = 0.1)
+sequenceRossler(t,cvec)
+dev.off()
+
